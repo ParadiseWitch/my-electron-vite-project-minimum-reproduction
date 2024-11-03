4131e59fb10bda908e486339c2671c2b613e399d
Maiiiiiid <zhp92@outlook.com>
2024-11-03 20:55:27 +0800

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+
+defineProps<{ msg: string }>()
+
+const openBrowser = async () => {
+  window.ipcRenderer.send('openBrowser')
+}
+</script>
+
+<template>
+  <div class="card">
+    <button type="button" @click="openBrowser" >openBrowser</button>
+  </div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
